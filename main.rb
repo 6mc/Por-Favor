9eb125ead869487f644b8ec07b037f6974504b9e
mcanwoo <super.can@bk.ru>
2016-03-22 16:38:14 +0200

    Test

---
+++ main.rb
@@ -0,0 +1,19 @@
+# Hello World Program in Ruby
+puts "Hello World!";
+print "parabens";
+
+def er(urkul,salah)
+    puts "mia amikoj"
+    toplam=urkul*salah;
+    return toplam;
+puts dellen;
+end
+a = gets.to_i
+puts "ikinsi"
+
+
+a.to_i
+puts "sayı:"
+y=a*a
+puts y
+
